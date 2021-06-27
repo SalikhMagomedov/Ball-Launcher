d574541848e2a7ef8d76a6e0847d979b752df799
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-06-27 08:25:41 +0300

    World & Screen Space

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -3,12 +3,20 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    private Camera _mainCamera;
+
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
     private void Update()
     {
         if (!Touchscreen.current.primaryTouch.press.IsPressed()) return;
         
         var touchPosition =  Touchscreen.current.primaryTouch.position.ReadValue();
+        var worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
 
-        Debug.Log(touchPosition);
+        Debug.Log(worldPosition);
     }
 }
